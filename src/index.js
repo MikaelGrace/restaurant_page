00a5fdc427593445a52f7da991ba2543bd8aa927
MikaelGrace <MikeOkoduwa700@gmail.com>
2026-01-17 22:38:04 +0100

    install the html loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 console.log("Welcome to the restaurant");
 import { InitialPageLoad } from "./initialpageload";
 import { ContactPage } from "./contacttab";
+import "./styles.css";
+import ResturantLogo from "./resturant.png";
 
+const logo = document.createElement("img");
 const divContent = document.querySelector("#content");
 
+logo.src = ResturantLogo;
+document.body.appendChild(logo);
 InitialPageLoad();
 const button = document.querySelector(".nav-btn");
 button.addEventListener("click", () => {
